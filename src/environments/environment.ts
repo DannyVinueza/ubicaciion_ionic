0f465ad95777c8f1e5ef945f7eaff1fd9003ab4e
DannyVinueza <danny.vinueza@epn.edu.ec>
2023-12-06 17:30:31 -0500

    Fin de guardar la ubicacion en el firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,15 +4,7 @@
 
 export const environment = {
   production: false,
-  firebaseConfig: {
-    apiKey: "AIzaSyAx1rhCDuGz0G7x5hngipWbm10_XYFlbwA",
-    authDomain: "bnch-9456f.firebaseapp.com",
-    projectId: "bnch-9456f",
-    storageBucket: "bnch-9456f.appspot.com",
-    messagingSenderId: "499402471592",
-    appId: "1:499402471592:web:c1812632203f748acf2a9d",
-    measurementId: "G-WD7306H8JM",
-}
+ 
 }
 
 
